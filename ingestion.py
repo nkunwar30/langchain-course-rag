ede3a999836dc3a42e3470374370bb09e448d21e
Niraj Kumar <nkunwar30@hotmail.com>
2026-03-22 07:31:17 +0530

    Enhance ingestion script to load, split, and embed documents using Langchain components. Added TextLoader for document ingestion, CharacterTextSplitter for chunking, and integrated OpenAIEmbeddings with PineconeVectorStore for storage. Updated print statements for better process visibility.

--- ingestion.py
+++ ingestion.py
@@ -1,10 +1,31 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
-    print(os.getenv("PINECONE_API_KEY"))
+    #print(os.getenv("PINECONE_API_KEY"))
+    loader = TextLoader("mediumblog1.txt")
+    documents = loader.load()
+
+    print("Splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+    texts = text_splitter.split_documents(documents)
+    print(f"Split into {len(texts)} chunks")
+    
+    
+    print("Embedding...")
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    vectorstore = PineconeVectorStore(index_name="mediumblog1", embedding=embeddings)
+    
+    print("Ingesting...")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=''
+    print("Done!")
 
