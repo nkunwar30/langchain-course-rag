f433c1cc2337b8180c05040fed5b320f2b8d1825
Niraj Kumar <nkunwar30@hotmail.com>
2026-03-23 06:27:47 +0530

    used Recursive splitter than regular

--- ingestion.py
+++ ingestion.py
@@ -4,14 +4,17 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-
+#from pinecone import Pinecone
 
 load_dotenv()
+#pc= Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+
+index_name = os.environ.get("INDEX_NAME")
 
 if __name__ == '__main__':
-    print("Ingesting...")
+    print("Loading data...")
     #print(os.getenv("PINECONE_API_KEY"))
-    loader = TextLoader("mediumblog1.txt")
+    loader = TextLoader("mediumblog1.txt", encoding='utf-8')
     documents = loader.load()
 
     print("Splitting...")
@@ -22,10 +25,10 @@ if __name__ == '__main__':
     
     
     print("Embedding...")
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-    vectorstore = PineconeVectorStore(index_name="mediumblog1", embedding=embeddings)
+    embeddings = OpenAIEmbeddings()
+    #vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
     
     print("Ingesting...")
-    PineconeVectorStore.from_documents(texts, embeddings, index_name=''
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=index_name)
     print("Done!")
 
